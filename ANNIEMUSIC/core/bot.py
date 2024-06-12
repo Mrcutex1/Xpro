e7a7c44b083fb905d4325a1ece632da7455693e6
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-06-13 01:06:25 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class JARVIS(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Annie is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="⏤͟͟͞ ♡︎ ˹ᴘʀᴏ sɪʀɪ ˼ 🫧🕊⃝",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
